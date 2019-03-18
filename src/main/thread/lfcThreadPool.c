1aea53099fc4a6115bbeda327ae50d01ced83c6e
Heiko Blobner <h.blobner@avm.de>
2019-03-18 16:13:46 +0100

    threadpool added

---
+++ src/main/thread/lfcThreadPool.c
@@ -0,0 +1,335 @@
+/*
+ * Copyright (c) 2016, Mathias Brossard <mathias@brossard.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file lfcThreadPool.c
+ * @brief Threadpool implementation file
+ */
+
+#include "lfcThreadPool.h"
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+typedef enum {
+    immediate_shutdown = 1,
+    graceful_shutdown  = 2
+} threadpool_shutdown_t;
+
+/**
+ *  @struct threadpool_task
+ *  @brief the work struct
+ *
+ *  @var function Pointer to the function that will perform the task.
+ *  @var argument Argument to be passed to the function.
+ */
+typedef struct {
+    void (*function)(void *);
+    void *argument;
+} threadpool_task_t;
+
+/**
+ *  @struct threadpool
+ *  @brief The threadpool struct
+ *
+ *  @var notify       Condition variable to notify worker threads.
+ *  @var threads      Array containing worker threads ID.
+ *  @var thread_count Number of threads
+ *  @var queue        Array containing the task queue.
+ *  @var queue_size   Size of the task queue.
+ *  @var head         Index of the first element.
+ *  @var tail         Index of the next element.
+ *  @var count        Number of pending tasks
+ *  @var shutdown     Flag indicating if the pool is shutting down
+ *  @var started      Number of started threads
+ */
+struct lfcThreadPool_ {
+    pthread_mutex_t lock;
+    pthread_cond_t notify;
+    pthread_t *threads;
+    threadpool_task_t *queue;
+    int thread_count;
+    int queue_size;
+    int head;
+    int tail;
+    int count;
+    int shutdown;
+    int started;
+};
+
+static int lfcThreadPool_free(
+    lfcThreadPool_t *pool
+) {
+    if(pool == NULL || pool->started > 0) {
+        return -1;
+    }
+
+    /* Did we manage to allocate ? */
+    if(pool->threads) {
+        free(pool->threads);
+        free(pool->queue);
+
+        /* Because we allocate pool->threads after initializing the
+           mutex and condition variable, we're sure they're
+           initialized. Let's lock the mutex just in case. */
+        pthread_mutex_lock(&(pool->lock));
+        pthread_mutex_destroy(&(pool->lock));
+        pthread_cond_destroy(&(pool->notify));
+    }
+    free(pool);
+    return 0;
+}
+
+/**
+ * @function void *threadpool_thread(void *threadpool)
+ * @brief the worker thread
+ * @param threadpool the pool which own the thread
+ */
+static void *lfcThreadPool_thread(
+    void *threadpool
+) {
+    lfcThreadPool_t *pool = (lfcThreadPool_t *)threadpool;
+    threadpool_task_t task;
+
+    for(;;) {
+        /* Lock must be taken to wait on conditional variable */
+        pthread_mutex_lock(&(pool->lock));
+
+        /* Wait on condition variable, check for spurious wakeups.
+           When returning from pthread_cond_wait(), we own the lock. */
+        while((pool->count == 0) && (!pool->shutdown)) {
+            pthread_cond_wait(&(pool->notify), &(pool->lock));
+        }
+
+        if((pool->shutdown == immediate_shutdown) ||
+            ((pool->shutdown == graceful_shutdown) &&
+                (pool->count == 0))) {
+            break;
+        }
+
+        /* Grab our task */
+        task.function = pool->queue[pool->head].function;
+        task.argument = pool->queue[pool->head].argument;
+        pool->head = (pool->head + 1) % pool->queue_size;
+        pool->count -= 1;
+
+        /* Unlock */
+        pthread_mutex_unlock(&(pool->lock));
+
+        /* Get to work */
+        (*(task.function))(task.argument);
+    }
+
+    pool->started--;
+
+    pthread_mutex_unlock(&(pool->lock));
+    pthread_exit(NULL);
+}
+
+/**
+ * @function threadpool_add
+ * @brief add a new task in the queue of a thread pool
+ * @param pool      Thread pool to which add the task.
+ * @param worker_fn Pointer to the function that will perform the task.
+ * @param argument  Argument to be passed to the function.
+ * @param flags     Unused parameter.
+ * @return 0 if all goes well, negative values in case of error (@see
+ * threadpool_error_t for codes).
+ */
+int lfcThreadPool_addWorker(
+    lfcThreadPool_t *pool,
+    void (*worker_fn)(void *),
+    void *arg,
+    int flags
+) {
+    int err = 0;
+    int next;
+    (void) flags;
+
+    if(pool == NULL || worker_fn == NULL) {
+        return threadpool_invalid;
+    }
+
+    if(pthread_mutex_lock(&(pool->lock)) != 0) {
+        return threadpool_lock_failure;
+    }
+
+    next = (pool->tail + 1) % pool->queue_size;
+
+    do {
+        /* Are we full ? */
+        if(pool->count == pool->queue_size) {
+            err = threadpool_queue_full;
+            break;
+        }
+
+        /* Are we shutting down ? */
+        if(pool->shutdown) {
+            err = threadpool_shutdown;
+            break;
+        }
+
+        /* Add task to queue */
+        pool->queue[pool->tail].function = worker_fn;
+        pool->queue[pool->tail].argument = arg;
+        pool->tail = next;
+        pool->count += 1;
+
+        /* pthread_cond_broadcast */
+        if(pthread_cond_signal(&(pool->notify)) != 0) {
+            err = threadpool_lock_failure;
+            break;
+        }
+    } while(0);
+
+    if(pthread_mutex_unlock(&pool->lock) != 0) {
+        err = threadpool_lock_failure;
+    }
+
+    return err;
+}
+
+/**
+ * @function threadpool_create
+ * @brief Creates a threadpool_t object.
+ * @param thread_count Number of worker threads.
+ * @param queue_size   Size of the queue.
+ * @param flags        Unused parameter.
+ * @return a newly created thread pool or NULL
+ */
+lfcThreadPool_t *lfcThreadPool_create (
+    int thread_count,
+    int queue_size,
+    int flags
+) {
+    lfcThreadPool_t *pool;
+    int i;
+    (void) flags;
+
+    if(thread_count <= 0 || thread_count > MAX_THREADS || queue_size <= 0 || queue_size > MAX_QUEUE) {
+        return NULL;
+    }
+
+    if((pool = (lfcThreadPool_t *)malloc(sizeof(lfcThreadPool_t))) == NULL) {
+        goto err;
+    }
+
+    /* Initialize */
+    pool->thread_count = 0;
+    pool->queue_size = queue_size;
+    pool->head = pool->tail = pool->count = 0;
+    pool->shutdown = pool->started = 0;
+
+    /* Allocate thread and task queue */
+    pool->threads = (pthread_t *)malloc(sizeof(pthread_t) * thread_count);
+    pool->queue = (threadpool_task_t *)malloc
+        (sizeof(threadpool_task_t) * queue_size);
+
+    /* Initialize mutex and conditional variable first */
+    if((pthread_mutex_init(&(pool->lock), NULL) != 0) ||
+        (pthread_cond_init(&(pool->notify), NULL) != 0) ||
+        (pool->threads == NULL) ||
+        (pool->queue == NULL)) {
+        goto err;
+    }
+
+    /* Start worker threads */
+    for(i = 0; i < thread_count; i++) {
+        if(pthread_create(&(pool->threads[i]), NULL,
+            lfcThreadPool_thread, (void *) pool) != 0) {
+            lfcThreadPool_destroy(pool, 0);
+            return NULL;
+        }
+        pool->thread_count++;
+        pool->started++;
+    }
+
+    return pool;
+
+err:
+    if(pool) {
+        lfcThreadPool_free(pool);
+    }
+    return NULL;
+}
+
+/**
+ * @function threadpool_destroy
+ * @brief Stops and destroys a thread pool.
+ * @param pool  Thread pool to destroy.
+ * @param flags Flags for shutdown
+ *
+ * Known values for flags are 0 (default) and threadpool_graceful in
+ * which case the thread pool doesn't accept any new tasks but
+ * processes all pending tasks before shutdown.
+ */
+int lfcThreadPool_destroy (
+    lfcThreadPool_t *pool,
+    int flags
+) {
+    int i, err = 0;
+
+    if(pool == NULL) {
+        return threadpool_invalid;
+    }
+
+    if(pthread_mutex_lock(&(pool->lock)) != 0) {
+        return threadpool_lock_failure;
+    }
+
+    do {
+        /* Already shutting down */
+        if(pool->shutdown) {
+            err = threadpool_shutdown;
+            break;
+        }
+
+        pool->shutdown = (flags & threadpool_graceful) ? graceful_shutdown : immediate_shutdown;
+
+        /* Wake up all worker threads */
+        if((pthread_cond_broadcast(&(pool->notify)) != 0) ||
+            (pthread_mutex_unlock(&(pool->lock)) != 0)) {
+            err = threadpool_lock_failure;
+            break;
+        }
+
+        /* Join all worker thread */
+        for(i = 0; i < pool->thread_count; i++) {
+            if(pthread_join(pool->threads[i], NULL) != 0) {
+                err = threadpool_thread_failure;
+            }
+        }
+    } while(0);
+
+    /* Only if everything went well do we deallocate the pool */
+    if(!err) {
+        lfcThreadPool_free(pool);
+    }
+    return err;
+}
--- src/test/runner.c
+++ src/test/runner.c
@@ -3,15 +3,34 @@
 #include <data/lfcStringOps.h>
 #include <malloc.h>
 #include <mem/lfcMemPool.h>
+#include <thread/lfcThreadPool.h>
 
 #define ALIGN(x)    (x + (__BIGGEST_ALIGNMENT__ - x % __BIGGEST_ALIGNMENT__))
 #define CALLOC_STATIC_MEM(name, size, value) \
         char name[size]; \
         memset(name, value, sizeof(name));
 
+void worker (
+    void *arg
+) {
+    fprintf(stderr, "%s@%d: %d\n", __func__,  __LINE__, (int)arg);
+}
+
 void runner_fn () {
-    char buf[5] = { (char)0xff };
-    lfcMemPool_t *tto = lfcMemPool_ctor(buf, sizeof(buf), memPool_WRITE_W_INITIALIZE);
+    lfcThreadPool_t *tp = lfcThreadPool_create(5, 10, 0);
+
+    lfcThreadPool_addWorker(tp, worker, (void *) 1, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 2, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 3, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 4, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 5, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 6, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 7, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 8, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 9, 0);
+    lfcThreadPool_addWorker(tp, worker, (void *) 0, 0);
+
+    lfcThreadPool_destroy(tp, threadpool_graceful);
 }
 
 int main (
