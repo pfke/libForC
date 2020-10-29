8acd7a264fe7f560a9f1b30aa682667efe09fcb0
Heiko Blobner <h.blobner@avm.de>
2020-10-29 17:45:43 +0100

    [data/lfcStringBuilder] .preproc-magic from headers removed

--- src/main/data/lfcStringBuilder.h
+++ src/main/data/lfcStringBuilder.h
@@ -12,28 +12,39 @@ extern "C" {
 
 /*--------------------------------------------------------------------------------------*\
 \*--------------------------------------------------------------------------------------*/
-lfcOOP_defineClass(lfcStringBuilder, lfcObject,
-    //-----------------------------------------------------------------------------
-    // FIELDS
-    //-----------------------------------------------------------------------------
+DEFINE_CLASS(lfcStringBuilder)
+
+struct lfcStringBuilder { const struct lfcObject _;
     lfcList_t *stringList;
+};
 
-    ,
-    //-----------------------------------------------------------------------------
-    // PUBLIC METHOD
-    //-----------------------------------------------------------------------------
-    int, append, (const char *in),
-    int, appendChar, (char in),
-    int, append_itsYourMemNow, (char *in),
+struct lfcStringBuilder_class { const struct lfcObject_class _;
+    method_t append;
+    method_t appendChar;
+    method_t append_itsYourMemNow;
+    method_t result;
+};
 
-    char *, result, ()
-    )
+struct lfcStringBuilder_methods {
+    int (*append)(lfcStringBuilder_t *self, const char *in);
+    int (*appendChar)(lfcStringBuilder_t *self, char in);
+    int (*append_itsYourMemNow)(lfcStringBuilder_t *self, char *in);
+
+    char * (*result)(lfcStringBuilder_t *self);
+
+    // super
+    const lfcObject_methods_t *base;
+};
 
 /**
  * Erzeugt eine lfcStringBuilderger Instanz.
  */
 lfcStringBuilder_t *lfcStringBuilder_ctor (void);
 
+int lfcStringBuilder_append(lfcStringBuilder_t *self, const char *in);
+int lfcStringBuilder_appendChar(lfcStringBuilder_t *self, char in);
+int lfcStringBuilder_append_itsYourMemNow(lfcStringBuilder_t *self, char *in);
+char * lfcStringBuilder_result(lfcStringBuilder_t *self);
 
 #ifdef __cplusplus
 }
